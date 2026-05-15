f47017fee445d2fca5a701d1a06b0e3f30a89fc7
김현민 <rlagusals911@naver.com>
2026-05-15 14:37:38 +0900

    CSRF 방어 설정

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -8,10 +8,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,6 +19,17 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthController implements AuthApi {
 
   private final AuthService authService;
+
+  @GetMapping(path = "csrf-token")
+  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+    String tokenValue = csrfToken.getToken();
+    log.debug("CSRF 토큰 요청: {}", tokenValue);
+
+      return ResponseEntity
+              .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+              .build();
+  }
+
 
   @PostMapping(path = "login")
   public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
