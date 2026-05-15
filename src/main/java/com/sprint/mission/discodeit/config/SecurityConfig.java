da3d8114ff5a54d58a16b1ed3893c0cd41bbb0df
김현민 <rlagusals911@naver.com>
2026-05-15 11:35:57 +0900

    Secure 설정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.sprint.mission.discodeit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http.build();
+    }
 }
