2882c0ba57ed527f848a4fccf805e0f50cc757f8
김현민 <rlagusals911@naver.com>
2026-05-18 10:23:04 +0900

    로그인 핸들러 작성

--- src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/security/SecurityConfig.java
@@ -12,13 +12,19 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            LoginSuccessHandler loginSuccessHandler,
+            LoginFailureHandler loginFailureHandler
+    ) throws Exception {
 
         http
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
                 .formLogin(login -> login
-                        .loginProcessingUrl("/api/auth/login"));
+                        .loginProcessingUrl("/api/auth/login")
+                        .successHandler(loginSuccessHandler)
+                        .failureHandler(loginFailureHandler));
         return http.build();
     }
 
